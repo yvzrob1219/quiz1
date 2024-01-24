80acbe85fa3e06f6df7edb0bf4b91729d07e9b46
yvzrob1219 <robbaban12@gmail.com>
2024-01-25 00:12:17 +0800

    added imag3es

---
+++ app/utils/memberinfo.tsx
@@ -0,0 +1,37 @@
+import { exportTraceState } from "next/dist/trace";
+
+const memberinfo = [
+    { 
+        name : 'Wendell Terence Dador',
+        position : 'Senior Developer',
+        motto: 'I code with a smile and a cup of coffee!',
+        link : ""
+    },
+    {
+        name: 'Robert Llyod Baban',
+        position: 'Team Leader',
+        motto: 'I make bugs disappear like magic!',
+        link:''
+    },
+   
+    { 
+        name: 'Onil Martinez',
+        position: 'Developer',
+        motto: 'I turn caffeine into code!',
+        link:''
+    },
+    {
+        name: 'Love Faith Alcorin',
+        position: 'Graphic Designer',
+        motto: 'I create designs that make people go "Wow!"',
+        link:''
+    },
+    {
+        name: 'Joshua Samenian',
+        position: 'Graphic Designer',
+        motto: 'I turn imagination into pixels!',
+        link:''
+    }
+];
+
+export default memberinfo
